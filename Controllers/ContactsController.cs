29dcb65a1a8000a716d68d2f81dc51f4cbc4bd47
Kharn Tupmongkol <kharnthorn@gmail.com>
2022-12-30 23:03:18 +0000

    Add the search function in contacts

--- Controllers/ContactsController.cs
+++ Controllers/ContactsController.cs
@@ -61,7 +61,7 @@ namespace ContactPro.Controllers
             //new view once a category is selected
             else
             {
-                contacts = appUser.Categories.FirstOrDefault(c => c.Id == categoryId)
+                contacts = appUser.Categories.FirstOrDefault(c => c.Id == categoryId)!
                                   .Contacts
                                   .OrderBy(c => c.LastName)
                                   .ThenBy(c => c.LastName)
@@ -71,6 +71,38 @@ namespace ContactPro.Controllers
             ViewData["CategoryId"] = new SelectList(categories, "Id", "Name");
 
             return View(contacts);
+        }
+
+        [Authorize]
+        public IActionResult SearchContacts(string searchString)
+        {
+            string appUserId = _userManager.GetUserId(User);
+            var contacts = new List<Contact> ();
+
+            AppUser appUser = _context.Users
+                                      .Include(c => c.Contacts)
+                                      .ThenInclude(c => c.Categories)
+                                      .FirstOrDefault(u => u.Id == appUserId)!;
+
+            if (String.IsNullOrEmpty(searchString))
+            {
+                contacts = appUser.Contacts
+                                  .OrderBy(c => c.LastName)
+                                  .ThenBy(c => c.FirstName)
+                                  .ToList();
+            }
+            //filter
+            else
+            {
+                contacts = appUser.Contacts
+                                  .Where(c => c.FullName!.ToLower().Contains(searchString.ToLower()))
+                                  .OrderBy(c => c.LastName)
+                                  .ThenBy(c => c.FirstName)
+                                  .ToList();
+            }
+            ViewData["CatagoryId"] = new SelectList(appUser.Categories, "Id", "Name", 0);
+
+            return View(nameof(Index), contacts);
         }
 
         // GET: Contacts/Details/5
