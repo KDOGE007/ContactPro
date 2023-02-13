451db523d3fbd2c2531252164bdf70febf6a8096
Kharn Tupmongkol <kharnthorn@gmail.com>
2023-02-13 23:07:51 +0000

    update settings

--- Helpers/ConnectionHelper.cs
+++ Helpers/ConnectionHelper.cs
@@ -6,17 +6,16 @@ namespace ContactPro.Helpers
     {
         public static string GetConnectionString(IConfiguration configuration)
         {
-            var connectionString = configuration.GetSection("pgSettings")["pgConnection"];
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
             var databaseUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
-
             return string.IsNullOrEmpty(databaseUrl) ? connectionString : BuildConnectionString(databaseUrl);
         }
 
-        //build a connection string from the environment. i.e., Heroku, Railway
+        //build the connection string from the environment. i.e. Heroku
         private static string BuildConnectionString(string databaseUrl)
         {
             var databaseUri = new Uri(databaseUrl);
-            var userInfo = databaseUri.UserInfo.Split(",");
+            var userInfo = databaseUri.UserInfo.Split(':');
             var builder = new NpgsqlConnectionStringBuilder
             {
                 Host = databaseUri.Host,
@@ -27,7 +26,6 @@ namespace ContactPro.Helpers
                 SslMode = SslMode.Require,
                 TrustServerCertificate = true
             };
-
             return builder.ToString();
         }
     }
